45b7fa9aaa50907b8edf07145f3cd04daf0f34f3
dembbuto <dembbuto@naver.com>
2024-06-02 21:32:23 +0900

    [blog] 이전, 다음 포스트 카드

--- src/service/posts.ts
+++ src/service/posts.ts
@@ -11,7 +11,11 @@ export type Post = {
   featured: boolean;
 }
 
-export type PostData = Post & { content: string };
+export type PostData = Post & {
+  content: string;
+  next: Post | null;
+  prev: Post | null;
+};
 
 export async function getFeaturedPosts(): Promise<Post[]> {
   return getAllPosts()
@@ -32,13 +36,17 @@ export async function getAllPosts(): Promise<Post[]> {
 
 export async function getPostData(fileName: string): Promise<PostData> {
   const filePath = path.join(process.cwd(), 'data', 'posts', `${fileName}.md`);
-  const metadata = await getAllPosts()
-    .then((posts) => posts.find((post) => post.path === fileName));
+  const posts = await getAllPosts();
+  const post = posts.find((post) => post.path === fileName);
 
-  if (!metadata) {
+  if (!post) {
     throw new Error(`${fileName}에 해당하는 포스트가 없습니다.`);
   }
 
+  const index = posts.indexOf(post);
+  const next = index > 0 ? posts[index - 1] : null;
+  const prev = index < posts.length - 1 ? posts[index + 1] : null;
+
   const content = await readFile(filePath, 'utf-8');
-  return { ...metadata, content };
+  return { ...post, content, next, prev };
 }
